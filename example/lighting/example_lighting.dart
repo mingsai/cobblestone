90f5e418ca88e768c3796ba876388be04f491212
Ethan T <ETundo15@gmail.com>
2018-04-15 15:14:29 -0500

    Made the lighting demo very satisfying

--- example/lighting/example_lighting.dart
+++ example/lighting/example_lighting.dart
@@ -13,9 +13,14 @@ class GeometryExample extends BaseGame {
   Texture wall;
   Texture wallNorm;
 
-  List<Vector3> lightPos = [new Vector3(0.0, 0.0, 0.075), new Vector3(0.0, 0.0, 0.075),
-    new Vector3(0.0, 0.0, 0.075), new Vector3(0.0, 0.0, 0.075)];
-  List<Vector4> lightColor = [Colors.mediumVioletRed, Colors.orange, Colors.greenYellow, Colors.aliceBlue];
+  num time = 0.0;
+
+  int numLights = 10;
+  List<Vector3> lightPosData = [];
+  List<Vector4> lightColorInit = [];
+
+  List<Vector3> lightPos = [];
+  List<Vector4> lightColor = [];
 
   Vector4 ambientColor = new Vector4(0.6, 0.6, 1.0, 0.001);
   Vector3 attenuation = new Vector3(0.4, 3.0, 20.0);
@@ -26,6 +31,18 @@ class GeometryExample extends BaseGame {
     renderer = new SpriteBatch(gl, assetManager.get("lighting"));
 
     gl.setGLViewport(canvasWidth, canvasHeight);
+
+    for(int i = 0; i < numLights; i++) {
+      var newPos = new Vector3.random();
+      newPos.scale(2.0);
+      newPos.sub(new Vector3.all(2.0));
+      lightPosData.add(newPos);
+      lightColorInit.add(new Vector4.random());
+
+      lightPos.add(new Vector3.random());
+      lightPos[i].z = 0.075;
+      lightColor.add(new Vector4.zero());
+    }
 
     wall = assetManager.get("tileDiffuse");
     wallNorm = assetManager.get("tileNormal");
@@ -66,15 +83,30 @@ class GeometryExample extends BaseGame {
 
   @override
   update(num delta) {
-    if(mouse.x != null) {
-      lightPos[0].x = mouse.x.toDouble() / width;
-      lightPos[0].y = mouse.y.toDouble() / height;
-      lightPos[1].x = mouse.x.toDouble() / width;
-      lightPos[1].y = (height - mouse.y.toDouble()) / height;
-      lightPos[2].x = (width - mouse.x.toDouble()) / width;
-      lightPos[2].y = mouse.y.toDouble() / height;
-      lightPos[3].x = (width - mouse.x.toDouble()) / width;
-      lightPos[3].y = (height - mouse.y.toDouble()) / height;
+    time += delta;
+    for(int i = 0; i < numLights; i++) {
+      lightPos[i].x = lightPos[i].x + lightPosData[i].x * delta;
+      lightPos[i].y = lightPos[i].y + lightPosData[i].y * delta;
+      if(lightPos[i].x > 1) {
+        lightPos[i].x = 1.0;
+        lightPosData[i].x = -1.0;
+      }
+      if(lightPos[i].x < 0) {
+        lightPos[i].x = 0.0;
+        lightPosData[i].x = 1.0;
+      }
+      if(lightPos[i].y > 1) {
+        lightPos[i].y = 1.0;
+        lightPosData[i].y = -1.0;
+      }
+      if(lightPos[i].y < 0) {
+        lightPos[i].y = 0.0;
+        lightPosData[i].y = 1.0;
+      }
+
+      lightColor[i].r = cos(time);
+      lightColor[i].g = sin(time);
+      lightColor[i].b = sin(time);
     }
   }
 
